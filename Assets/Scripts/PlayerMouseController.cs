c246d67129769d85421214c8f9e3a3b8f5d03551
shellka <Leonid.v.brish@gmail.com>
2013-01-20 18:16:30 +0400

    [ADD] Добавил перемещение игрока по клику
    Игрок теперь перемещается при клике мышкой через НавАгент.
    Обновил параметры камеры, теперь она зафиксирована над игроком под
    углом.

--- Assets/Scripts/PlayerMouseController.cs
+++ Assets/Scripts/PlayerMouseController.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class PlayerMouseController : MonoBehaviour {
 	
 	private Ray ray;
-	private float hitdist = 0.0f;
+	private float hitdist = 18f;
 	private NavMeshAgent nav;
 	
 	// Use this for initialization
@@ -17,10 +17,18 @@ public class PlayerMouseController : MonoBehaviour {
 		
 		if ( Input.GetMouseButton( 1 )) { 
 			
+			/**if ( this.nav.remainingDistance > 0 ) {
+				Debug.Log ( "already moving" );
+				return;
+			}**/
+			
 			this.ray = Camera.main.ScreenPointToRay ( Input.mousePosition );
 			
 			this.nav.enabled = true;
+			//Debug.Log ("Ray is: " + ray.ToString( ));
+			Debug.Log ("Ray point: " + this.ray.GetPoint( this.hitdist ));
 			this.nav.SetDestination( this.ray.GetPoint( this.hitdist ));
+			//this.nav.SetDestination( new Vector3( 874.1208f, 4.563694f, 700.4479f ));
 			
 		}
 		
